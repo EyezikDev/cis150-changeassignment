7a5f5c392a04a2d68c286279fc7ca57c51d4b6b9
Isaac Robinson <Isaacrobinsonhh@gmail.com>
2021-11-14 01:55:25 -0500

    Finished

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
-DIVIDER = "\n[]----------------------------------------------------------------[]\n"
+import os
 
-coins = [25, 10, 5, 1]
-coinsReturn = []
+DIVIDER = "\n[]----------------------------------------------------------------[]\n"
+QUARTER = 25
+DIME = 10
+NICKLE = 5
+PENNIE = 1
+
 floatingPrice = 0
 intPrice = 0
-
+changeTotal = 0
 quarters = 10
 dimes = 10
 nickels = 10
 pennies = 10
+running = True
+valid = True
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 print(DIVIDER)
 print('            Welcome to the Virtual Vending Machine!')
 print(f"\n     Stock Start: {quarters} quarters, {dimes} dimes, {nickels}, and {pennies} pennies")
 print(DIVIDER)
 
-changeTotal = 0
-money = ()
-running = True
 while running:
-    if input("Would you like to purchase an item? y/n > ").upper() == "Y":
-        valid = True
+    userInput = input("Would you like to purchase an item? y/n > ").upper()
+    if userInput == "Y":
         while valid:
             try:
-                isZeroOrNeg = True
-                while isZeroOrNeg:
-                    floatingPrice = float(input("Please enter a purchase price in floating-point form > "))
-                    if floatingPrice > float(0):
-                        isZeroOrNeg = False
-                    elif floatingPrice == float(0):
+                isNotIncorrect = True
+                while isNotIncorrect:
+                    floatingPrice = int(float(input("Please enter a purchase price in floating-point form > ")) * 100)
+                    if floatingPrice > 0:
+                        isNotIncorrect = False
+                    elif floatingPrice == 0:
                         print("Incorrect Format! Can not be 0")
-                    elif floatingPrice < float(0):
+                    elif floatingPrice < 0:
                         print("Incorrect Format! Can not be negative")
-                isZeroOrNeg = True
-                while isZeroOrNeg:
-                    intPrice = int(input("Please enter a purchase price in integer form > "))
+                isNotIncorrect = True
+                while isNotIncorrect:
+                    intPrice = int(input("Please enter a purchase price in integer form > ")) * 100
                     if intPrice > 0:
-                        isZeroOrNeg = False
-                    elif intPrice == float(0):
+                        isNotIncorrect = False
+                    elif intPrice == 0:
                         print("Incorrect Format! Can not be 0")
-                    elif intPrice < float(0):
+                    elif intPrice < 0:
                         print("Incorrect Format! Can not be negative")
                     else:
                         print("Incorrect Format! Can not be 0")
-                changeTotal = (intPrice - floatingPrice)
-                changeTotal = round(changeTotal, 2) * 100
+                changeTotal = intPrice - floatingPrice
             except ValueError:
                 print("Incorrect Format!")
                 continue
@@ -52,9 +59,45 @@ while running:
                 valid = False
         print(DIVIDER)
         print(f"Change difference: {round(changeTotal / 100, 2)}")
-
+        coinsUsed = 0
+        quartersUsed = 0
+        dimesUsed = 0
+        nickelsUsed = 0
+        penniesUsed = 0
+        while changeTotal >= QUARTER and quarters > 0:
+            quartersUsed += 1
+            quarters -= 1
+            changeTotal -= QUARTER
+            coinsUsed += 1
+        print(f"Quarter(s)       > {quartersUsed}")
+        while changeTotal >= DIME and dimes > 0:
+            dimesUsed += 1
+            dimes -= 1
+            changeTotal -= DIME
+            coinsUsed += 1
+        print(f"Dime(s)          > {dimesUsed}")
+        while changeTotal >= NICKLE and nickels > 0:
+            nickelsUsed += 1
+            nickels -= 1
+            changeTotal -= NICKLE
+            coinsUsed += 1
+        print(f"Nickle(s)        > {nickelsUsed}")
+        while changeTotal > PENNIE and pennies > 0:
+            penniesUsed += 1
+            pennies -= 1
+            changeTotal -= PENNIE
+            coinsUsed += 1
+        print(f"Pennie(s)        > {penniesUsed}")
+        print(f"Coin(s)          > {coinsUsed}")
+        coinTracker = quarters + nickels + dimes + pennies
         print(DIVIDER)
-        print(f"\n     Stock After: {quarters} quarters, {dimes} dimes, {nickels}, and {pennies} pennies")
+        if coinTracker == 0:
+            print(f"     Stock Is Empty, Goodbye.")
+            running = False
+        else:
+            print(f"     Stock After: {quarters} quarters, {dimes} dimes, {nickels} nickels, and {pennies} pennies")
         print(DIVIDER)
-    else:
+    elif userInput == "N":
         running = False
+    else:
+        continue
